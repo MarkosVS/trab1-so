9e86225dade6c2768e3f614f9474b05b15ba9662
Marcos Vinicius Sombra <markos.vs@live.com>
2018-04-25 10:31:55 -0300

    add linha de execução cliente e outras coisinhas

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
-#include <iostream>
-#include <stdlib.h>
-#include <thread>
+#include <bits/stdc++.h>
 
 #define NUM_THREADS 100
+#define VALOR 10000
 
 using namespace std;
+// variaveis de controle da região critica
+mutex mtx;
+condition_variable cond_var;
+volatile int atual = 0;
+
+// variaveis compartilhadas
+volatile int requisicao = 0;
+volatile int resposta = 0;
+volatile int id_thread = -1;
 
 // função de somatorio
 int somatorio(int n){
+    // (n * (n+1))/2
     int soma = n;
     for(int i = 0; i < n; i++)
         soma += i;
@@ -17,12 +26,39 @@ int somatorio(int n){
 
 // linha de execução da thread cliente
 void cliente(int id){
-    int r = rand()%10000;
-    cout << "Thread " << id << ": " << r << endl;
+    // trava a si mesma, esperando a vez
+    unique_lock<mutex> trava(mtx);
+    // thread fica esperando ser a sua vez
+    while(id != atual)
+        cond_var.wait(trava);
+
+    // thread vê que é sua vez e sai do while
+    atual++;
+
+    // processo da thread
+    requisicao = rand() % VALOR + 1;
+
+    // testes
+    cout << "Thread: ";
+    cout << id + 1 << ".";
+    cout << " Atual = ";
+    cout << atual << endl;
+    cout << requisicao << endl;
+
+    // thread passa a vez
+    requisicao = 0;
+    cond_var.notify_all();
+}
+
+// linha de execução do servidor
+void servidor(int id){
+    cout << "Deu certo\n";
 }
 
 int main(){
-    // criação das threads
+    // criação da thread servidor
+    thread server = thread(servidor, -1);
+    // criação das threads cliente
     thread clientes[NUM_THREADS];
     for(int i = 0; i < NUM_THREADS; i++)
         clientes[i] = thread(cliente, i);
