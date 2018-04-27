a7bbd33f920e4e2a6e459eba599c07b2715f6993
Marcos Vinicius Sombra <markos.vs@live.com>
2018-04-27 09:52:24 -0300

    finalizando comunicação de processos

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ using namespace std;
 mutex mtx;
 condition_variable cond_var;
 volatile int atual = 0;
+volatile bool flag = false;
+volatile int cont_thread = 0;
 
 // variaveis compartilhadas
 volatile int requisicao = 0;
 volatile int resposta = 0;
-volatile int id_thread = -1;
 
 // função de somatorio
 int somatorio(int n){
@@ -43,16 +44,32 @@ void cliente(int id){
     cout << id + 1 << ".";
     cout << " Atual = ";
     cout << atual << endl;
-    cout << requisicao << endl;
+    cout << "Valor requisitado: " << requisicao << endl;
+
+    // thread chama o servidor e espera a resposta
+    flag = true;
+    while(flag);
+
+    // cliente recebe a resposta e imprime o valor
+    cout << "Valor respondido: " << resposta << endl << endl;
 
     // thread passa a vez
-    requisicao = 0;
     cond_var.notify_all();
 }
 
 // linha de execução do servidor
 void servidor(int id){
-    cout << "Deu certo\n";
+    while(true){
+        if(flag){
+            resposta = somatorio(requisicao);
+            cont_thread++;
+            flag = false;
+        }
+
+        // condição de parada do servidor
+        if(cont_thread == NUM_THREADS)
+            break;
+    }
 }
 
 int main(){
